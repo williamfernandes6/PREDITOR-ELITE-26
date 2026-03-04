93d8a21628423fc255dc6b542319490e762d2292
williamfernandes6 <williamsquad6@gmail.com>
2026-03-04 15:00:43 +0100

    Update service worker cache version and logic
    Update cache version and add activate event listener for cache cleanup.

--- sw.js
+++ sw.js
@@ -1,9 +1,24 @@
-const CACHE_NAME = 'pe-av26-v2';
+const CACHE_NAME = 'pe-av26-v4';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      // Faz cache apenas do index.html para não simplificar nada
       return cache.addAll(['./index.html']);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
