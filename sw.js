38da75dcd143a59b429ec6f9009a6582e6808689
williamfernandes6 <williamsquad6@gmail.com>
2026-06-30 01:09:38 +0100

    Implement caching for assets in service worker
    Added asset caching during service worker installation and cleanup of old caches on activation.

--- sw.js
+++ sw.js
@@ -1,3 +1,29 @@
 const CACHE_NAME = 'pe-av26-v12';
-self.addEventListener('install', (e) => e.waitUntil(caches.open(CACHE_NAME)));
-self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
+const ASSETS = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
